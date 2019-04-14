9eeafb956a2852dfbb68a44a14d65d8bc69d1bb8
Vinícius de Souza Pereira <vinpxk@gmail.com>
2019-04-14 01:46:04 -0300

    Change media queries, add simple button

--- cloud/cloud.js
+++ cloud/cloud.js
@@ -1,13 +1,26 @@
+// add listener for scrolling effect on whole cloud section
 let cloudItems = document.querySelectorAll(".cloud-item");
 
 window.addEventListener("scroll", (event) => {
     cloudItems.forEach((item) => {
         // gets the position of the item within the screen - 0 is when it is around the top
-        let position = item.getBoundingClientRect().top;
-        // console.log("pos: " + position + " | body: " + document.body.scrollTop);
+        let position = item.getBoundingClientRect().top + document.documentElement.scrollTop;
+        console.log("pos: " + position + " | body: " + document.body.scrollTop);
 
         // add/remove the scroll effect class for the fade in effect
-        if(position < 800) item.classList.remove("scroll-effect");
-        else item.classList.add("scroll-effect");
-    })
+        if(position < 800) item.classList.remove("cloud-scroll-effect");
+        else item.classList.add("cloud-scroll-effect");
+    });
+});
+
+// button click effect
+let cloudButtons = document.querySelectorAll(".cloud-button");
+
+cloudButtons.forEach((button) => {
+    button.addEventListener("mousedown", (event) => {
+        button.id = "cloud-button-clicked";
+    });
+    button.addEventListener("mouseup", (event) => {
+        button.id = "";
+    });
 });
