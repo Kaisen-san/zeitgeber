98f9a3d168109d62a6cb5a2c8a86a98286b8a768
Vinícius de Souza Pereira <vinpxk@gmail.com>
2019-04-17 19:24:58 -0300

    Changes to carousel code, add test buttons

--- carousel/carousel.js
+++ carousel/carousel.js
@@ -1,40 +1,65 @@
-// let imgs = document.querySelectorAll(".carousel-image");
+// select the images and buttons in the page
+const images = Array.from(document.querySelectorAll(".carousel-image"));
+const buttons = Array.from(document.querySelectorAll(".carousel-button"));
 
-// imgs.forEach( img => img.addEventListener('click', event => {
+// function that moves the images
+const warp = function(event, position){
+    // makes it so the current middle image being clicked moves to the next image
+    if(Array.from(event.target.classList).includes("carousel-middle")) position++;
+    if(position > images.length - 1) position = 0;
 
-// }));
-// img.addEventListener('click', event => {
-//     img.classList.add("rotate");
-// });
+    // adjusts position for the other side images
+    let leftPosition = position - 1;
+    let rightPosition = position + 1;
+    if(leftPosition < 0) leftPosition = images.length - 1;
+    if(rightPosition > images.length - 1) rightPosition = 0;
+    
+    // hides all images
+    images.forEach( img => {
+        img.classList.add("carousel-hide");
+        img.classList.remove("carousel-left", "carousel-middle", "carousel-right");
+    });
 
-let left = document.querySelector(".left");
-let middle = document.querySelector(".middle");
-let right = document.querySelector(".right");
+    // sets position for the select images
+    images[position].classList.add("carousel-middle");
+    images[position].classList.remove("carousel-hide");
+    images[leftPosition].classList.add("carousel-left");
+    images[leftPosition].classList.remove("carousel-hide");
+    images[rightPosition].classList.add("carousel-right");
+    images[rightPosition].classList.remove("carousel-hide");
+};
 
-let images = Array.from(document.querySelectorAll(".carousel-image"));
+// add the listeners
+for (let i = 0; i < images.length; i++) {
+    images[i].addEventListener("click", (event) => {
+        warp(event, i);
+    });
+    buttons[i].addEventListener("click", (event) => {
+        warp(event, i);
+    });
+}
 
-images.forEach( img => img.addEventListener('click', function (event) {
-    let position = img.classList[img.classList.length-1];
-    console.log(position);
+// images.forEach( img => img.addEventListener('click', function (event) {
+//     let position = img.classList[img.classList.length-1];
 
-    if (position === "right") {
-        let imageAux = images.shift();
-        images.push(imageAux);
-    } else if (position === "left") {
-        let imageAux = images.pop();
-        images.unshift(imageAux);
-    }
+//     if (position === "right" || position === "middle") {
+//         let imageAux = images.shift();
+//         images.push(imageAux);
+//     } else if (position === "left") {
+//         let imageAux = images.pop();
+//         images.unshift(imageAux);
+//     }
 
-    images[0].classList.add("left");
-    images[0].classList.remove("middle", "right", "hide");
-    images[1].classList.add("middle");
-    images[1].classList.remove("left", "right", "hide");
-    images[2].classList.add("right");
-    images[2].classList.remove("left", "middle", "hide");
+//     images[0].classList.add("left");
+//     images[0].classList.remove("middle", "right", "hide");
+//     images[1].classList.add("middle");
+//     images[1].classList.remove("left", "right", "hide");
+//     images[2].classList.add("right");
+//     images[2].classList.remove("left", "middle", "hide");
     
 
-    for (let i = 3; i < images.length; i++) {
-        images[i].classList.add("hide");
-        images[i].classList.remove("left", "middle", "right");
-    }
-}));
+//     for (let i = 3; i < images.length; i++) {
+//         images[i].classList.add("hide");
+//         images[i].classList.remove("left", "middle", "right");
+//     }
+// }));
