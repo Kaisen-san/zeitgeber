0602e9f3630080fc8598462ec1f66ed956105fa8
Felipe Andrade <fasa.work@gmail.com>
2019-04-17 21:00:49 -0300

    Add cloud section

--- public/js/index.js
+++ public/js/index.js
@@ -89,3 +89,31 @@ for (let i = 0; i < images.length; i++) {
 //         images[i].classList.remove("left", "middle", "right");
 //     }
 // }));
+
+
+// add listener for scrolling effect on whole cloud section
+let cloudItems = document.querySelectorAll(".cloud-item");
+
+window.addEventListener("scroll", (event) => {
+    cloudItems.forEach((item) => {
+        // gets the position of the item within the screen - 0 is when it is around the top
+        let position = item.getBoundingClientRect().top;// + document.documentElement.scrollTop;
+        // console.log("pos: " + position + " | body: " + document.body.scrollTop);
+
+        // add/remove the scroll effect class for the fade in effect
+        if(position < 800) item.classList.remove("cloud-scroll-effect");
+        else item.classList.add("cloud-scroll-effect");
+    });
+});
+
+// button click effect
+let cloudButtons = document.querySelectorAll(".cloud-button");
+
+cloudButtons.forEach((button) => {
+    button.addEventListener("mousedown", (event) => {
+        button.id = "cloud-button-clicked";
+    });
+    button.addEventListener("mouseup", (event) => {
+        button.id = "";
+    });
+});
