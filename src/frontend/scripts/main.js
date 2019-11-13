62b178e1e3cd3919db64bfdea8a9c5b5da5d1317
Felipe Andrade <CORP\felipe.andrade>
2019-11-13 11:50:31 -0300

    Remove unnecessary code

--- src/frontend/scripts/main.js
+++ src/frontend/scripts/main.js
@@ -25,20 +25,4 @@
 
   fadeInEffectNodes.forEach( e => zeitgeber.fadeIn( ...e ) );
 
-  const contactForm = document.querySelector('.contact__form');
-
-  contactForm.addEventListener('submit', async (evt) => {
-    evt.preventDefault();
-
-    const inputs = zeitgeber.extractRequestPayload(contactForm, 'input', 'value');
-    const textareas = zeitgeber.extractRequestPayload(contactForm, 'textarea', 'value');
-
-    await zeitgeber.sendCaptchaRequest('contact', '/contact', {
-      ...inputs,
-      ...textareas
-    });
-
-    window.location.href = window.location.pathname;
-  });
-
 })();
